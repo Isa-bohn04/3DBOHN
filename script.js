4b033fbc71fe34bac35a655c205ab185ae350e72
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2026-03-18 14:21:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function irParaSecao(pagina){
 document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+      
             behavior: 'smooth'
         });
     });
